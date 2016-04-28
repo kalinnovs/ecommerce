6f0e52e3d295a4add4f6b72fc1fc835923c27006
Dipankar Acharjaya <dacharjaya@Dipankars-MacBook-Pro-3.local>
2016-04-28 12:30:37 -0700

    Updating service and home page style

--- dist/build/js/feature.js
+++ dist/build/js/feature.js
@@ -113,6 +113,16 @@ angular.module('eCommerce', ['ui.router','ui.bootstrap','firebase'])
           }
         }
       })
+      .state('productTree', {
+        url:'/productTree',
+        views: {
+          '': { 
+            templateUrl: 'app/components/inventory/productTree.html',
+            controller: 'productTreeCtrl',
+            controllerAs: 'productTreeCtrl'
+          }
+        }
+      })
     ;
 
     // We need to setup some parameters for http requests
@@ -912,14 +922,14 @@ angular.module('eCommerce')
 angular.module('eCommerce')
   .service('productTreeService', ['$http', function ($http) {
     var service = this;
-    // var BASE_URL = "http://ec2-52-32-195-43.us-west-2.compute.amazonaws.com/HaastikaWebService/admin";
+    var BASE_URL = "http://ec2-52-33-88-59.us-west-2.compute.amazonaws.com/HaastikaWebService/admin";
     // var BASE_URL = "http://17.168.48.250:8080/HaastikaDataService/admin";
-    var BASE_URL = "assets/json/productTree.json";
+    // var BASE_URL = "assets/json/productTree.json";
     // var BASE_URL = 'http://127.0.0.1:8081';
 
     //simply returns the category list
     service.list = function () {
-        return $http.get(BASE_URL ); // + "/categorytree"
+        return $http.get(BASE_URL + "/categorytree"); // + "/categorytree"
     }
 
     service.savecategory = function (category) {
