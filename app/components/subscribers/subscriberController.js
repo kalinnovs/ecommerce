360536a760675e9593b18d3097df88c6329e1241
pdwibedi <pdwibedi@apple.com>
2016-05-30 01:11:23 -0700

    Added admin menu

--- app/components/subscribers/subscriberController.js
+++ app/components/subscribers/subscriberController.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('eCommerce')
-  .controller('SubscriberCtrl', function ($scope, $rootScope, $location, UserService, $http, SERVICE_URL) {
+  .controller('SubscriberCtrl', function ($scope, $rootScope, $location, $timeout, UserService, $http, SERVICE_URL) {
     var subscriber = this;
     var scoper = $scope;
     // debugger;
+    window.dataLoaded = false;
 
-    $rootScope.navigation = UserService.get().data.pageNavigation.categories;
-
-    UserService.GetAll( SERVICE_URL + 'subscribers')
+    UserService.GetAll( SERVICE_URL + '/subscribers')
         .then(function(data) {
+          // debugger;
           subscriber.data = data;
+          // $scope.$broadcast('dataloaded');
         })
         .catch(function(error) {
             //
@@ -20,6 +21,20 @@ angular.module('eCommerce')
         })
 
     $scope.approvePromo = function(elem) {
+        var data = {};
+        data.emailId = $(event.target).parents("tr").find(".email").html();
+        data.promoUsed = $(event.target).parents("tr").find(".promocode").html();
+        UserService.Update( SERVICE_URL + '/admin/updateSubscriber', data)
+        .then(function(data) {
+          debugger;
+        })
+        .catch(function(error) {
+            //
+        })
+        .finally(function() {
+            //
+        })
+
         var className = $(elem.target).hasClass("fa-toggle-off");
         if(className) {
             $(elem.target).removeClass("fa-toggle-off").addClass("fa-toggle-on");
